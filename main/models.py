7d975c92c565bc1d82c1d8f9951ac150da19ad00
andresduarter22 <andresduarter13@gmail.com>
2020-03-20 01:13:33 -0400

    Creating models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+class Usuario(models.Model):
+    ID
+    nombre = models.CharField(max_length=200)
+    correo
+
+    def __str__(self):
+        return self.name
+class Formulario(models.Model):
+    ID = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    ID_animal
+    fecha
+    zona
+    celular
+class Animal(models.Model):
+    ID_animal
+    raza
+    sexo
+    tamanio
+    edad
+    foto
+    estado
+class Telefono(models.Model):
+    ID
+    celular
