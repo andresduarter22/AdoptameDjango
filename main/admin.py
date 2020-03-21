6a1154c08d93d5e7758570121065182c60ed3a70
andresduarter22 <andresduarter13@gmail.com>
2020-03-20 20:47:21 -0400

    database register to admin path

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import User, Animal, Form, PhoneNumber
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Animal)
+admin.site.register(Form)
+admin.site.register(PhoneNumber)
+
