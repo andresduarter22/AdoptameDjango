1444ec2803d3f747bc2b58816b95c8b8e0fc2cee
andresduarter22 <andresduarter13@gmail.com>
2020-03-24 19:32:07 -0400

    urls fixed

--- main/urls.py
+++ main/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns =[
     path("", views.home, name="home"),
-    path("", views.homeLog, name="homeLog"),
-    path("", views.profile, name="profile"),
-    path("", views.home, name="home")
+    path("homeLog/", views.homeLog, name="homeLog"),
+    path("profile/", views.profile, name="profile"),
+    path("search/", views.search(), name="search")
 ]
