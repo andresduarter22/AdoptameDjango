4b42a11ca2b9b5534fb631af68304b4f2bc279b3
andresduarter22 <andresduarter13@gmail.com>
2020-03-24 21:07:35 -0400

    href 2

--- main/urls.py
+++ main/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-    path("", views.home, name="home"),
+    path("home/", views.home, name="home"),
     path("homeLog/", views.homeLog, name="homeLog"),
     path("profile/", views.profile, name="profile"),
     path("search/", views.search, name="search")
